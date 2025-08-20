5e4c17ebc68f6947a92abd1a60b871d254c13bcb
uni-kakurenbo <kohma1116@gmail.com>
2025-08-20 19:58:44 +0900

    setup: refactor, forbidden

--- src/app/api/files/[id]/route.ts
+++ src/app/api/files/[id]/route.ts
@@ -1,4 +1,5 @@
 import { getFileBlob } from "@/actions/getFileBlob";
+import { getFileMeta } from "@/actions/getFileMeta";
 import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,7 +7,23 @@ type Params = {
 	id: string;
 };
 
-export async function GET(_: NextRequest, { params }: { params: Promise<Params> }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<Params> }) {
+	const headers = request.headers;
 	const { id } = await params;
-	return new NextResponse(await getFileBlob(id).catch(notFound));
+
+	headers.set(
+		"Content-Type",
+		await getFileMeta(id)
+			.then(meta => meta.mime)
+			.catch(notFound)
+	);
+
+	return new NextResponse(
+		await getFileBlob(id)
+			.then(blob => blob.stream())
+			.catch(notFound),
+		{
+			headers,
+		}
+	);
 }
--- src/components/TraqImage.tsx
+++ src/components/TraqImage.tsx
@@ -1,15 +1,22 @@
 import { getFilePath } from "@/lib/client";
-import { Image, ImageProps } from "@heroui/image";
+import { Image, ImageProps } from "@/components/Image";
+import { getImageSize } from "@/actions/getImageSize";
 
 type Props = {
 	fileId: string;
 } & Omit<ImageProps, "src">;
 
-export default async function TraqImage({ fileId, alt, ...props }: Props) {
+export default async function TraqImage({ fileId, alt, width, height, fill, ...props }: Props) {
+	if (!fill && (!width || !height)) {
+		const { width: defaultWidth, height: defaultHeight } = await getImageSize(fileId);
+		width ??= defaultWidth;
+		height ??= defaultHeight;
+	}
+
 	return (
 		<Image
 			src={getFilePath(fileId)}
-			alt={alt}
+			{...{ alt, fill, width, height }}
 			{...props}
 		/>
 	);
