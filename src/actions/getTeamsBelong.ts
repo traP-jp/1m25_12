0e61e166d871cd46327201e105a50b681ad3b75c
ebisen1444 <0294ryotaro@gmail.com>
2025-09-03 09:56:25 +0900

    チーム所属一覧取得actionを作成 group/{groupIdを叩いて作りました}

--- src/actions/getTeamsBelong.ts
+++ src/actions/getTeamsBelong.ts
@@ -1,9 +1,15 @@
 import { getUserInfo } from "./traq/users";
+import { getGroup } from "./traq/groups";
 
 export async function getTeamsBelong(userId: string) {
 	try {
 		const userInfo = await getUserInfo(userId);
-		return userInfo.groups;
+		const teamNames = [];
+		for (const groupId of userInfo.groups) {
+			const teamName = (await getGroup(groupId)).name;
+			teamNames.push(teamName);
+		}
+		return teamNames;
 	} catch (error) {
 		console.error("Error fetching user belonging teams:", error);
 		throw error;
