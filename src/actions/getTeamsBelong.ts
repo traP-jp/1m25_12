4aa74c749183d447fcb011b983dccefeb5ad3d4f
uni-kakurenbo <kohma1116@gmail.com>
2025-09-03 23:25:07 +0900

    refactor

--- src/actions/getTeamsBelong.ts
+++ src/actions/getTeamsBelong.ts
@@ -1,17 +1,13 @@
 import { getUserInfo } from "./traq/users";
 import { getGroup } from "./traq/groups";
 
-export async function getTeamsBelong(userId: string) {
-	try {
-		const userInfo = await getUserInfo(userId);
-		const teamNames = [];
-		for (const groupId of userInfo.groups) {
-			const teamName = (await getGroup(groupId)).name;
-			teamNames.push(teamName);
-		}
-		return teamNames;
-	} catch (error) {
-		console.error("Error fetching user belonging teams:", error);
-		throw error;
-	}
+export async function getUserTeams(userId: string) {
+	const { groups } = await getUserInfo(userId);
+
+	return await Promise.all(
+		groups.map(async group => {
+			const { name } = await getGroup(group);
+			return name;
+		})
+	);
 }
