fe7eab0747383bc67bad994c4cce106b964ea0cb
uni-kakurenbo <kohma1116@gmail.com>
2025-09-06 16:26:11 +0900

    test

--- src/actions/loadWorks.ts
+++ src/actions/loadWorks.ts
@@ -8,7 +8,11 @@ export async function loadWorks(formData: FormData | string) {
 	const channelId =
 		typeof formData === "string" ? formData : (formData.get("channel_id") as string);
 
+	console.log(channelId);
+
 	const messages = await getChannelMessages(channelId);
+
+	console.log(messages);
 
 	await prisma.channel.upsert({
 		where: { id: channelId },
@@ -16,15 +20,25 @@ export async function loadWorks(formData: FormData | string) {
 		create: { id: channelId },
 	});
 
-	await prisma.userChannel.createMany({
-		data: messages.map(({ channelId, userId }) => ({ channelId, ownerId: userId })),
-		skipDuplicates: true,
-	});
+	console.log(messages.map(({ userId }) => ({ channelId, ownerId: userId })));
 
 	await prisma.userChannel.createMany({
 		data: messages.map(({ userId }) => ({ channelId, ownerId: userId })),
 		skipDuplicates: true,
 	});
+
+	console.log(
+		messages.map(({ id, userId, createdAt, updatedAt }) => ({
+			id,
+			category: Category.OTHERS,
+			viewCount: 0,
+			allowReviews: false,
+			createdAt: new Date(createdAt),
+			updatedAt: new Date(updatedAt),
+			channelId,
+			authorId: userId,
+		}))
+	);
 
 	await prisma.work.createMany({
 		data: messages.map(({ id, userId, createdAt, updatedAt }) => ({
