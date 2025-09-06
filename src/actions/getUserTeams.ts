58190bc7c36187a9f5dd23fd5775eef758a4ce3d
uni-kakurenbo <kohma1116@gmail.com>
2025-09-06 09:54:30 +0900

    refactor

--- src/actions/getUserTeams.ts
+++
@@ -1,9 +0,0 @@
-import { getUserInfo } from "./traq/users";
-// import { getGroup } from "./traq/groups";
-// import { use } from "react";
-
-export async function getUserTeams(userId: string) {
-	const user_groups: string[] = [];
-	user_groups.push(...(await getUserInfo(userId)).groups);
-	return user_groups;
-}
--- src/app/(main)/users/[name]/page.tsx
+++ src/app/(main)/users/[name]/page.tsx
@@ -9,7 +9,7 @@ import { prisma } from "@/lib/prisma";
 import { Link } from "@/components/Link";
 import { WorksTabs } from "@/components/WorksTabs";
 import TeamIcon from "@/components/TeamIcon";
-import { getUserTeams } from "@/actions/getUserTeams";
+import { TEAM_LIST } from "@/lib/constants";
 
 type Params = {
 	name: string;
@@ -18,7 +18,7 @@ type Params = {
 export default async function UserPage({ params }: { params: Promise<Params> }) {
 	const { name } = await params;
 	const { id, displayName, iconFileId } = await getUser(name).catch(notFound);
-	const { bio } = await getUserInfo(id).catch(notFound);
+	const { bio, groups } = await getUserInfo(id).catch(notFound);
 
 	const channels = await prisma.userChannel.findMany({
 		where: { ownerId: id },
@@ -28,17 +28,6 @@ export default async function UserPage({ params }: { params: Promise<Params> }) 
 	const channelPaths = await Promise.all(
 		channels.map(channel => getChannelPath(channel.channelId))
 	);
-
-	const userTeamIds = await getUserTeams(id);
-	const ALL_TEAMS = [
-		{ id: "867b3529-696f-4bd1-af53-1947eba92e77", name: "graphics" },
-		{ id: "cb977ab2-85fa-4953-ac4d-809eaef427e6", name: "sound" },
-		{ id: "280bf56d-fa22-46bc-8dcc-6367d600d873", name: "algorithm" },
-		{ id: "af240e80-8526-4f21-925e-b20eded06284", name: "game" },
-		{ id: "ec54d385-e5e7-4554-8aa2-878ebedc9db0", name: "kaggle" },
-		{ id: "f86db5ec-dc02-4885-aa0a-732bb229a1b5", name: "sysad" },
-		{ id: "c5670065-75d4-4851-bfba-9ff05201fc44", name: "ctf" },
-	];
 
 	return (
 		<div>
@@ -79,8 +68,8 @@ export default async function UserPage({ params }: { params: Promise<Params> }) 
 							<div className="justify-center text-left">
 								<span className="text-xl">所属</span>
 								<ul className="flex flex-wrap gap-2 mt-2">
-									{ALL_TEAMS.map(team => {
-										const isMember = userTeamIds.includes(team.id);
+									{TEAM_LIST.map(team => {
+										const isMember = groups.includes(team.id);
 										return (
 											<li
 												key={team.id}
